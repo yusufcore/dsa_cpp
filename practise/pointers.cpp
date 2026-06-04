84ffdefb18dfd7c68e9cacc503bba73b8ee6e2d7
yusuf-in <mohammadyusuf.work@gmail.com>
2026-06-04 04:56:14 +0000

    array inside heap

--- practise/pointers.cpp
+++ practise/pointers.cpp
@@ -16,20 +16,42 @@
 
 
 // Pointer to an array
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+    
+//     int A[5] = {2,4,6,8,10};
+//     int *p;
+//     // int *p = &A[0];  //pointer to the first element of the array
+//     p = A; 
+
+//     for(int i=0; i<5; i++){
+//         // cout << A[i] << endl;
+//         cout << p[i] << endl; // pointer arithmetic to access elements of the array using the pointer variable
+
+//     }
+
+//     return 0;
+
+// }
+
+
+// Array inside Heap
 #include <iostream>
-using namespace std;
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;   
 
 int main(){
-    
-    int A[5] = {2,4,6,8,10};
+
     int *p;
-    // int *p = &A[0];  //pointer to the first element of the array
-    p = A; 
+    p=(int *)malloc(5*sizeof(int));  // allocating memory for an array of 5 integers in the heap using malloc function.
+
+    p[0] = 10; p[1] = 20; p[2] = 30; p[3] = 40; p[4] = 50;
 
     for(int i=0; i<5; i++){
-        cout << A[i] << endl;
+        cout << p[i] << endl;
     }
-
     return 0;
-
 }
