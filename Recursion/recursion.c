dd746054c507852ffa29133a0c89454d0b62580b
yusuf-in <mohammadyusuf.work@gmail.com>
2026-06-06 15:01:48 +0000

    recursion program

--- Recursion/recursion.c
+++ Recursion/recursion.c
@@ -1,15 +1,12 @@
-#include <stdio.h>
 
-void fun(int n)
-{
-    if(n > 0)
-    {
+#include <stdio.h>
+void fun(int n) {
+    if (n > 0) {
         printf("%d ", n);
-        fun(n-1);
-    }   
+        fun(n - 1);
+    }
 }
-int main()
-{
+int main() {
     int n = 5;
     fun(n);
     return 0;
