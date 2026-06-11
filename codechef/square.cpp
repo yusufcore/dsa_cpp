7c32643d67d0c7ae7c46855e92185b55f3a58ebf
yusuf-in <mohammadyusuf.work@gmail.com>
2026-06-11 14:24:19 +0000

    solved my first codechef question

---
+++ codechef/square.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    int d = 4;
+    int e = 5;
+
+    cout << a << " - " << a*a << endl;
+    cout << b << " - " << b*b << endl;
+    cout << c << " - " << c*c << endl;
+    cout << d << " - " << d*d << endl;
+    cout << e << " - " << e*e << endl;
+
+}
