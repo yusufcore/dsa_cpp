1ae0ef34203607cfedd54ce1fd9b32faf9d37c35
yusufcore <mohammadyusuf.work@gmail.com>
2026-06-12 15:11:13 +0000

    increasing, decreasing question solved

---
+++ codechef/inc_dec.cpp
@@ -0,0 +1,17 @@
+// increasing, decreasing, or neither program
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    int c;
+    cin >> a >> b >> c;
+    if(a<b && b <c){
+        cout << "Increasing " <<endl;
+    }else if(a>b && b<c){
+        cout << "Decreasing " <<endl;
+    }else{
+        cout << "Neither" <<endl;
+    }
+}
