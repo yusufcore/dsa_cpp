cae4cc5a786d9e2458e0c6e43ab1a9ad8d6b82a9
yusufcore <mohammadyusuf.work@gmail.com>
2026-06-15 06:03:43 +0000

    learnt about maps and thier implementations

---
+++ STL/unordered_map.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main(){
+    map<int, string> m;
+    m[1] = "Yusuf",
+    m[5] = "Ahemed",
+    m[3] = "Ammar";
+    m.insert({4, "Aisha"});      // key -> value
+    map<int, string> :: iterator it;  // iterator is used to point to the element in the map.
+    for(it=m.begin(); it!=m.end(); it++){
+        cout << (*it).first << " " << (*it).second << endl;  // * is used to point to the value stored in the pointing element.
+    }
+
+}
