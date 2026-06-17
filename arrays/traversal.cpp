a416c97f3a594d735b7e81ab3bf2dc108aa4f619
yusufcore <mohammadyusuf.work@gmail.com>
2026-06-17 14:58:25 +0000

    traversed arrays

---
+++ arrays/traversal.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[] {3,4,5,68,9,7,4,32,5,7,8,90,8654,321};
+    int n = sizeof(arr)/sizeof(arr[0]);  // standard way to get size.
+
+    //forward
+    for(int i = 0; i<n; i++){
+        cout << arr[i] <<endl;;
+    }
+    //backward
+    for(int i = n-1; i>=0 ; i--){
+        cout << arr[i] <<endl;
+    }
+}
