57acfc80be4c8d4969a4c0f5e37f77f268f4eaea
yusufcore <mohammadyusuf.work@gmail.com>
2026-06-17 15:01:06 +0000

    *****

---
+++ STL/nested_vector.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printVec(vector<pair<int,int>> v){
+    cout << "Size: " << v.size() << endl;
+    for(int i =0; i<v.size();i++){
+        // v.size() -> O(1)
+        cout << v[i].first << " " << v[i].second <<endl;
+    }
+}
+int main(){
+    vector<pair<int,int>> v;
+    printVec(v);
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        int x, y;
+        cin >> x >> y;
+        v.push_back({x,y});
+    }
+    printVec(v);
+    
+}
