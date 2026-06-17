62a5794ba1586c1dd692c80c67e30a2b269fb016
yusufcore <mohammadyusuf.work@gmail.com>
2026-06-17 15:04:12 +0000

    studied and implemented unordered mapps

--- STL/unordered_map.cpp
+++ STL/unordered_map.cpp
@@ -8,9 +8,11 @@ int main(){
     m[5] = "Ahemed",
     m[3] = "Ammar";
     m.insert({4, "Aisha"});      // key -> value
-    map<int, string> :: iterator it;  // iterator is used to point to the element in the map.
-    for(it=m.begin(); it!=m.end(); it++){
-        cout << (*it).first << " " << (*it).second << endl;  // * is used to point to the value stored in the pointing element.
-    }
+    // map<int, string> :: iterator it;  // iterator is used to point to the element in the map.
+    // for(it=m.begin(); it!=m.end(); it++){
+    //     cout << (*it).first << " " << (*it).second << endl;  // * is used to point to the value stored in the pointing element.
 
+    for(auto pr : m){  // auto is used to automatically determine the type of the variable pr.
+        cout << pr.first << " " << pr.second << endl;  // no need to use * to point to the value stored in the pointing element.
+    }
 }
