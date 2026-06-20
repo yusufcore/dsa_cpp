6a12cdf5b857825717069e54f0386f5f2818c27f
yusufcore <mohammadyusuf.work@gmail.com>
2026-06-20 16:11:10 +0000

    practised arrays

---
+++ arrays/arr2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[4] = {3, 6, 9, 12};
+
+    for(int i = 0; i < 4; i++){
+        cout << arr[i] * 2 << endl;
+    }
+    return 0;
+}
+
